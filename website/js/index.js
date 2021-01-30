4e17705622a429d67740ebca3d5135424826ad06
unknown <kartikbansal2000@gmail.com>
2021-01-31 01:20:19 +0530

    Frontend Build Done

--- website/js/index.js
+++ website/js/index.js
@@ -1,4 +1,5 @@
   document.addEventListener('DOMContentLoaded', function() {
+    var date = new Date()
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {
       constrainWidth:false,
@@ -6,9 +7,18 @@
       coverTrigger: false
     });
 
-    var elems = document.querySelectorAll('.tabs');
-    var instance = M.Tabs.init(elems, {
-      swipeable: true
+    var chip_elems = document.querySelectorAll('.chips');
+    var instances = M.Chips.init(chip_elems, {
+      placeholder:"Enter Tech Stack",
+      secondaryPlaceholder:"Enter Tech Stack"
+    });
+
+    var date_elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(date_elems, {
+      minDate: date,
+      autoClose: true,
+      yearRange:0
     });
 
   });
+  // ember773
